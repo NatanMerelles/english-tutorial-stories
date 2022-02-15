fe83ecccfcaa6f59f6f77bc15b83d6c10e40d7fe
Natan <natanmerelles@gmail.com>
2022-02-15 19:04:03 -0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import Stories from 'react-insta-stories';
+
+
+
+const stories = [
+  {
+    url: '/img1.jpg',
+    duration: 5000,
+    header: {
+      heading: 'Natan Merelles',
+      subheading: 'Posted 10 de fev 2022',
+      profileImage: 'https://cdn-icons-png.flaticon.com/512/147/147144.png',
+    },
+  }, {
+    url: '/img2.jpg',
+    duration: 5000,
+    header: {
+      heading: 'Natan Merelles',
+      subheading: 'Posted 10 de fev 2022',
+      profileImage: 'https://cdn-icons-png.flaticon.com/512/147/147144.png',
+    },
+  }, {
+    url: '/img3.jpg',
+    duration: 5000,
+    header: {
+      heading: 'Natan Merelles',
+      subheading: 'Posted 10 de fev 2022',
+      profileImage: 'https://cdn-icons-png.flaticon.com/512/147/147144.png',
+    },
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='story-wrapper'>
+        <Stories
+          stories={stories}
+          onStoryStart={(a) => console.log({ a })}
+          storyContainerStyles={{ borderRadius: '6px' }}
+          defaultInterval={5000}
+          width='100%'
+          height='100%'
+          loop
+        />
+      </div>
     </div>
   );
 }
